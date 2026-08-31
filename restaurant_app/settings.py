5271713e39d43e9ccf771d48027488ecee4c551a
github_username <github_email>
2026-08-31 10:40:15 +0300

    Initial commit

--- restaurant_app/settings.py
+++ restaurant_app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ps0+38z67t%(yo=&u!^smqs58mer(vf$h)d6xap5+b7!$pcds@'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-ps0+38z67t%(yo=&u!^smqs58mer(vf$h)d6xap5+b7!$pcds@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST", "127.0.0.1")]
 
 
 # Application definition
